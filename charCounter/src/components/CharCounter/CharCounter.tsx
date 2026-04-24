c232ddb7c553bf03896712db17610c6fb3a54ccf
Fabiola <fabiola.aurelien@gmail.com>
2026-04-24 15:34:01 -0400

    Added additional interface to this component to display optional character count properties

--- charCounter/src/components/CharCounter/CharCounter.tsx
+++ charCounter/src/components/CharCounter/CharCounter.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import type {  TextStats } from '../../types';
+import type { CharacterCounterProps, TextStats } from '../../types';
 
-export const CharCounter: React.FC<TextStats> = ({ characterCount, wordCount, readingTime }) => {
+
+type Props = CharacterCounterProps & TextStats;
+
+export const CharCounter: React.FC<Props> = ({ characterCount, wordCount, readingTime, minWords=200, maxWords=1000, targetReadingTime = 5 }) => {
   return (
     <div>
     <p>Character Count: {characterCount}</p>
     <p>Word Count: {wordCount}</p>
     <p>Reading Time: {readingTime}</p>
+    <p>Min: {minWords} words | Max: {maxWords} words | Target Reading Time: {targetReadingTime} minutes</p>
     </div>
   );
 };

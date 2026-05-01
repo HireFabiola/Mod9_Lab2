7f73a6f4e6e75f6c63e51a1b1c092c113e5ca9b3
Fabiola <fabiola.aurelien@gmail.com>
2026-05-01 17:52:58 -0400

    Added comments to CharCounter

--- charCounter/src/components/CharCounter/CharCounter.tsx
+++ charCounter/src/components/CharCounter/CharCounter.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import type { CharacterCounterProps, TextStats } from '../../types';
 import { StatsDisplay } from '../StatsDisplay/StatsDisplay';
 
-
+// Created new Prop type to find a way to use the TextStats prop in the function call
 type Props = CharacterCounterProps & TextStats;
 
 export const CharCounter: React.FC<Props> = ({ characterCount, wordCount, readingTime, minWords = 200, maxWords = 400, targetReadingTime = 2 }) => {
+    
+    // Created a new object destructured from the ChaarterCountProps interface
     const newObj = { characterCount, wordCount, readingTime }
     return (
         <>

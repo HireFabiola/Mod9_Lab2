b658f76576700991001ca9c617519a869ec7ea30
Fabiola <fabiola.aurelien@gmail.com>
2026-05-01 17:57:33 -0400

    Added comments to TextInput

--- charCounter/src/components/TextInput/TextInput.tsx
+++ charCounter/src/components/TextInput/TextInput.tsx
@@ -1,10 +1,13 @@
 import type { TextInputProps } from '../../types';
 
+// Function declaration
 export const TextInput: React.FC<TextInputProps> = ({
+  // expected props
   onTextChange,
   placeholder = 'Start typing...',
   initialValue = ''
 }) => {
+  // Return input field that reads user input
   return (
     <div className="max-w-md mx-auto mt-6">
       <textarea
